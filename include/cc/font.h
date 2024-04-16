ef558d93320f85f1792358b412b4a293c52c92dd
Jakit Liang <liangjie@jimuyida.com>
2024-04-16 16:13:49 +0800

    add font class

---
+++ include/cc/font.h
@@ -0,0 +1,14 @@
+//
+// Created by liangjie on 2024/4/16.
+//
+
+#ifndef CHOCO_CPP_FONT_H
+#define CHOCO_CPP_FONT_H
+
+namespace CC {
+    struct Font {
+
+    };
+}
+
+#endif //CHOCO_CPP_FONT_H
---
+++ src/font.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liangjie on 2024/4/16.
+//
+
+#include "font.h"
